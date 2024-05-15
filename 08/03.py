8f7cd99fcdd4e98cec73106472c7e542e195a4d0
Ars11k <159742907+Ars11k@users.noreply.github.com>
2024-05-15 11:57:06 +0500

    Add files via upload

---
+++ 08/03.py
@@ -0,0 +1,7 @@
+username = input()
+for char in username:
+    if not char.isalnum() and char != '_':
+        print("Неверный символ:", char)
+        break
+else:
+    print("OK")
---
+++ 08/09.py
@@ -0,0 +1,5 @@
+N = int(input())
+for _ in range(N):
+    line = input().strip()
+    if not line.startswith("####") and not line.startswith("%%"):
+        print(line)
